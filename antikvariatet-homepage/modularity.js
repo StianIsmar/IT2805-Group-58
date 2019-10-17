f1ee9342839d13644da079d996e7560a3eeece97
StianIsmar <41960375+StianIsmar@users.noreply.github.com>
2019-10-17 18:13:14 +0200

    Added modularity for navbar and footer

--- antikvariatet-homepage/modularity.js
+++ antikvariatet-homepage/modularity.js
@@ -1,4 +1,7 @@
 onload = () => {
   document.getElementById("footer").innerHTML =
     '<div><footer><div class="left"></div><div class="right"><div style="width: 720px"><iframe width="720" height="400" src="https://maps.google.com/maps?width=720&amp;height=400&amp;hl=en&amp;q=Nedre%20Bakklandet%204%2C%207014%20Trondheim+(Antikvariatet)&amp;ie=UTF8&amp;t=&amp;z=16&amp;iwloc=B&amp;output=embed" frameborder="0"scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">gps coordinates</a></iframe></div><br/></div></footer></div>';
+
+  document.getElementById("navigationBar").innerHTML =
+    '<div class = "navbar"><div class = "kontakt"> Kontakt oss</div><div class = "restaurant"> Restaurant</div><div class = "arrangementer"> Arrangementer</div></div>';
 };
