0c8087a2d8888c87aa8e2bd8cb7fb164f5b05144
mortenoosvik <morten.o.osvik@gmail.com>
2019-10-25 18:49:13 +0200

    Fixed Navbar and added comments

--- antikvariatet-homepage/modularity.js
+++ antikvariatet-homepage/modularity.js
@@ -1,21 +1,37 @@
 onload = () => {
-  document.getElementById("footer").innerHTML =
-    '<div><footer><div class="left"></div><div class="right"><div style="width: 720px"><iframe width="720" height="400" src="https://maps.google.com/maps?width=720&amp;height=400&amp;hl=en&amp;q=Nedre%20Bakklandet%204%2C%207014%20Trondheim+(Antikvariatet)&amp;ie=UTF8&amp;t=&amp;z=16&amp;iwloc=B&amp;output=embed" frameborder="0"scrolling="no" marginheight="0" marginwidth="0"><a href="https://www.maps.ie/coordinates.html">gps coordinates</a></iframe></div><br/></div></footer></div>';
-
+  
+  // Modular header. Includes NavBar
   document.getElementById("header").innerHTML =
     '<h1>ANTIKVARIATET</h1>' +
     '<div id="nav">' +
-        '<a id="About" class="" href="Homepage.html">About</a>'+
-        '<a id="Events" class="" href="Events.html">Events</a>'+
-        '<a id="Restaurant" class="" href="Restaurant.html">Restaurant</a>'+
-        '<a id="Contact" class="" href="Contact.html">Contact</a>'+
-    '</div>'
+        '<a id="About" href="Homepage.html">About</a>'+
+        '<a id="Events" href="Events.html">Events</a>'+
+        '<a id="Restaurant" href="Restaurant.html">Restaurant</a>'+
+        '<a id="Contact" href="Contact.html">Contact</a>'+
+    '</div>';
 
   // Get underline on current page in navbar
   var title = document.title;
   var underlineNav = document.getElementById(title);
   underlineNav.className = "active";
 
-  
-
+  // Modular footer
+  document.getElementById("footer").innerHTML =
+    '<div>' +
+      '<footer>'+
+        '<div class="left"></div>'+
+        '<div class="right">'+
+          '<div style="width: 720px">'+
+            '<iframe width="720" height="400"'+
+            'src="https://maps.google.com/maps?width=720&amp;height=400&amp;hl=en&amp;'+
+            'q=Nedre%20Bakklandet%204%2C%207014%20Trondheim+(Antikvariatet)&amp;ie=UTF8&amp;t=&amp;z=16&amp;iwloc=B&amp;output=embed" '+
+            'frameborder="0"scrolling="no" marginheight="0" marginwidth="0">'+
+              '<a href="https://www.maps.ie/coordinates.html">gps coordinates</a>'+
+            '</iframe>'+
+          '</div>'+
+          '<br/>'+
+        '</div>'+
+      '</footer>'+
+    '</div>';
+    
   };
