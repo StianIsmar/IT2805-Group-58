90cc69e0f4cf06a922811ed4fb60f1221700a417
opheliap <opheliap@stud.ntnu.no>
2019-11-06 08:54:52 +0100

    onscroll, restaurant &about us

--- antikvariatet-homepage/onscroll.js
+++ antikvariatet-homepage/onscroll.js
@@ -1,40 +1,16 @@
-/*function debounce(func, wait = 10, immediate = true) {
-    let timeout;
-    return function() {
-        let context = this, args = arguments;
-        let later = function() {
-            timeout = null;
-            if (!immediate) func.apply(context,args);
-        };
-        let callNow = immediate && !timeout;
-        clearTimeout(timeout);
-        timeout = setTimeout(later, wait);
-        if (callNow) func.apply(context,args);
-    };
-};
-let scrollPos = 0;
-const article = document.querySelector('.about');
-
-function checkPosition() {
-    let windowY = window.scrollY;
-    if (windowY < scrollPos) {
-        //Scroll up
-        article.classList.add('is-hidden');
-        article.classList.remove('is-visible');
-    } else {
-        //scrolldown
-        article.classList.add('is-visible');
-        article.classList.remove('is-hidden');
+window.addEventListener("scroll", function () {displayOnScroll()});
+function displayOnScroll() {
+    let about = document.getElementById("about").className;
+    if (about != "fadeIn") {
+        if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+            document.getElementById("about").className = "fadeIn";
+        } else {
+            document.getElementById("about").className = "fadeOut";
+        }
     }
-    scrollPos = windowY;
 }
-window.addEventListener('scroll',debounce(checkPosition));*/
 
-window.addEventListener("scroll", function () {displayAbout()});
-function displayAbout() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        document.getElementById("about").className = "fadeIn";
-    } else {
-        document.getElementById("about").className = "fadeOut";
-    }
+function getAbout() {
+    window.scrollTo(0,200);
+    document.getElementById("about").className = "fadeIn";
 }
