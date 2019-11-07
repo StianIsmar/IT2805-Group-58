07a459074eb02400d1fb3ad492dbd5c1213ae782
opheliap <opheliap@stud.ntnu.no>
2019-11-07 18:39:03 +0100

    Changing pictures for more visible tekst

--- antikvariatet-homepage/js-functions/appear_on_scroll.js
+++ antikvariatet-homepage/js-functions/appear_on_scroll.js
@@ -9,9 +9,3 @@ function displayOnScroll() {
         }
     }
 }
-
-function getAbout() {
-    let headerheight = document.getElementById("header").offsetHeight;
-    window.scrollTo(0,headerheight);
-    document.getElementById("about").className = "fadeIn";
-}
